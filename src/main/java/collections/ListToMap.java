e3d555d7a2217134b5438f23ec2d9fc2dd18cec3
Aparna I <aparna.i@sap.com>
2017-10-24 08:47:15 +0530

    Changed file

--- src/main/java/collections/ListToMap.java
+++ src/main/java/collections/ListToMap.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
+               
 public class ListToMap {
 
 	public static void main(String[] args) {
